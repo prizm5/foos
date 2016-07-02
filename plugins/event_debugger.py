f2725b608d1341be76da49a4bd6a54e597bacf3e
Raspberry Pi <pi@rpi>
2016-07-02 16:59:04 -0400

    some poking around

--- plugins/event_debugger.py
+++ plugins/event_debugger.py
@@ -6,7 +6,9 @@ logger = logging.getLogger(__name__)
 class Plugin:
     def __init__(self, bus):
         self.log_events = ['decrement_score', 'goal_event',
-                           'people_start_playing', 'people_stop_playing']
+                           'people_start_playing', 'people_stop_playing','goal_event',
+                           'increment_score','score_goal',
+                           'score_changed', 'win_game','goal_event']
         bus.subscribe(self.process_event, thread=True, subscribed_events=self.log_events)
 
     def process_event(self, ev):
--- plugins/physical.py
+++ plugins/physical.py
@@ -8,39 +8,30 @@ class Plugin():
     def __init__(self, bus):
         self.bus = bus
         self.bus.subscribe(self.process_event, thread=True)
-        AddButton(17, self.handle_red_button,500)
+        GPIO.setmode(GPIO.BCM)
+        self.AddButton(17, self.handle_red_button,500)
+        self.bus.notify("movement_detected")
+        self.bus.notify("people_start_playing")
+        self.bus.notify("set_game_mode", {"mode": 10, "timeout": 20 })
 
     def process_event(self, ev):
         now = time.time()
-        if ev.name == "score_changed":
 
     def run(self):
         while True:
-          while not self.queue.empty():
-            ev = self.queue.get_nowait()
-            self.process_event(ev)
-          time.sleep(0.01)
+            time.sleep(0.01)
     
     def handle_red_button(self, channel):
-        self.logger.info("Red button pushed")
-        if self.__state != GameState.idle:
-            if self.__state == GameState.live_game:
-                self.messages.send_game_queued(self.game.id)
-            self.__state = GameState.idle
-
-    def team1Scored(self):
-      self.bus.notify('black_minus')
+        self.bus.notify('increment_score', {'team': 'black'})
 
     def AddSensor(self, port, callback, bounce):
         p = int(port)
         b = int(bounce)
-        self.logger.info("Sensor added: Port: %s | Callback: %s | Bounce: %s", p, callback, b)
         GPIO.setup(p, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.add_event_detect(p, GPIO.FALLING, callback=callback, bouncetime=b)
 
     def AddButton(self, port, callback, bounce):
         p = int(port)
         b = int(bounce)
-        self.logger.info("Button added: Port: %s | Callback: %s | Bounce: %s", p, callback, b)
         GPIO.setup(p, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.add_event_detect(p, GPIO.FALLING, callback=callback, bouncetime=b)
