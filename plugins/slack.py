75ed2ca5de9e46edf665375b80f4d2a2e82e7c32
Nils Creque <nils.creque@smartstream-stp.com>
2016-08-31 18:48:21 -0400

    fixing

--- plugins/slack.py
+++ plugins/slack.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
 
-import time
-import queue
-from threading import Thread
-from pusher import Pusher
-import config
-import logging.config
-import pusherclient
-import json
-from pusher import Pusher
+import requests
 
 
-logging.config.dictConfig(config.log)
-logger = logging.getLogger(__name__)
 url = 'https://hooks.slack.com/services/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'
 
 payload={"text": "A very important thing has occurred! <https://alert-system.com/alerts/1234|Click here> for details!"}
