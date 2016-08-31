e95f3aa1ac1a91133c38fa316b71021657ac930f
Nils Creque <nils.creque@smartstream-stp.com>
2016-08-31 19:00:25 -0400

    fixing

--- plugins/slack.py
+++ plugins/slack.py
@@ -5,7 +5,9 @@ import json
 
 url = 'https://hooks.slack.com/services/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'
 
-payload={"text": "A very important thing has occurred! <https://alert-system.com/alerts/1234|Click here> for details!"}
+payload={
+    "username":"foosball",
+    "text": "We Have a Winner!!!"}
 
 headers = {'content-type': 'application/json'}
 
