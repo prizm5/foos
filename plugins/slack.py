7ae0867c48b7a836ac4b1bf81f4da9db2f2a4863
Nils Creque <nils.creque@smartstream-stp.com>
2016-09-01 08:25:22 -0400

    fixing

--- plugins/slack.py
+++ plugins/slack.py
@@ -5,17 +5,18 @@ import json
 
 url = 'https://hooks.slack.com/services/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'
 
-payload={
-    "username":"foosball",
-    "text": "We Have a Winner!!!"}
-
 headers = {'content-type': 'application/json'}
 msg = {
   "yellow": ["Nilhouse", "Keith"],
   "black": ["MrsHammer", "Maximus"],
   "mode": 5
 }
-winners = "{0} & {1}".format(msg.get('yellow')[0],msg.get('yellow')[1])
-payload.update({"winners": winners })
+
+winners = "We Have a Winner!!! \n {0} & {1}".format(msg.get('yellow')[0],msg.get('yellow')[1])
+payload={
+    "username":"foosball",
+    "text": winners}
+
+
 print(payload)
 #response = requests.post(url, data=json.dumps(payload), headers=headers)
