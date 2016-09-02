5a5abab9a5a948a73cd1cf8e0ff6aa4cbaf2b9ef
Raspberry Pi <pi@rpi>
2016-09-02 12:42:51 -0400

    updating launcher

--- launcher.sh
+++ launcher.sh
@@ -1,4 +1,8 @@
-cd /
+cd ~
+killall python
+killall chromium-browser
 cd dev/foos
-sudo python3 foos.py
-cd /
+sudo python3 foos.py &
+python -m SimpleHTTPServer 8000 &
+chromium-browser http://localhost:8000/web &
+ 
