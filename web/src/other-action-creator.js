b5e8a0f073da5e3632e13c1cc3151e2cfd3fc934
Keith O <keithernet@protonmail.com>
2016-09-06 21:04:49 -0400

    use jsonp to get around cors issue

--- web/src/other-action-creator.js
+++ web/src/other-action-creator.js
@@ -1,43 +1,60 @@
 var moment = require('moment');
-var request = require('superagent');
+var $ = require('jquery');
+
+function jsonp(start, end, cb) {
+  $.ajax({
+    url: 'http://www3.septa.org/hackathon/NextToArrive/',
+    data: {
+      req1: start,
+      req2: end,
+      req3: 1
+    },
+    dataType: 'jsonp',
+    success: function(trips) {
+      cb(trips);
+    },
+    error: function(error) {
+      console.log('error: ', error);
+    }
+  });
+}
 
 function getTrainsForUsers(store) {
-    let users = store.getState().game.team1.concat(store.getState().game.team2);
-    return Promise.all(users.map(user => getTrains(user.station, user)));
+  let users = store.getState().game.team1.concat(store.getState().game.team2);
+  return Promise.all(users.map(user => getTrains(user.station, user)));
 }
 
 function getTrains(station, user) {
-    return new Promise((resolve, reject) => {
-        request.get('http://www3.septa.org/hackathon/NextToArrive/Suburban%20Station/' + encodeURI(station) + '/1')
-            .set('Accept', 'application/json')
-            .end((err, succ) => {
-                resolve(Object.assign(succ.body[0], {
-                    user: user
-                }));
-            });
-    });
+  return new Promise((resolve, reject) => {
+    let cb = data => {
+      resolve(Object.assign(data[0], {
+        user: user
+      }));
+    };
+    jsonp('Suburban Station', station, cb)
+  });
 }
 
 function runOnTimer(fn, args, interval) {
-    (function runMe() {
-        fn.apply(null, args);
-        setTimeout(runMe, interval);
-    })()
+  (function runMe() {
+    fn.apply(null, args);
+    setTimeout(runMe, interval);
+  })()
 }
 
 export default function(store) {
-    // runOnTimer(() => {
-    //     store.dispatch({
-    //         type: 'time_update',
-    //         payload: moment().format('h:mm:ss')
-    //     })
-    // }, [], 1000);
+  // runOnTimer(() => {
+  //     store.dispatch({
+  //         type: 'time_update',
+  //         payload: moment().format('h:mm:ss')
+  //     })
+  // }, [], 1000);
 
-    runOnTimer(() => {
-        getTrainsForUsers(store)
-            .then(d => store.dispatch({
-                type: 'trains_loaded',
-                payload: d
-            }))
-    }, [], 30000);
+  runOnTimer(() => {
+    getTrainsForUsers(store)
+      .then(d => store.dispatch({
+        type: 'trains_loaded',
+        payload: d
+      }))
+  }, [], 5000);
 }
