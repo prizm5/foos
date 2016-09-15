b5482c03e88afd6d00583a8691ea570f6261d254
Raspberry Pi <pi@rpi>
2016-09-15 08:28:25 -0400

    changing startup

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,7 @@
 cd ~
-killall python
-killall chromium-browser
+sudo killall python
+sudo killall python3
+sudo killall chromium-browser
 cd dev/foos
 python -m SimpleHTTPServer 8000 &
 chromium-browser http://localhost:8000/web &
--- plugins/goalpic.py
+++ plugins/goalpic.py
@@ -6,7 +6,7 @@ from subprocess import call
 class Plugin:
     def __init__(self, bus):
         self.bus = bus
-        bus.subscribe_map( 'score_goal': lambda d: self.snap()}, thread=True)
+        bus.subscribe_map('score_goal': lambda d: self.snap()}, thread=True)
 
     def snap(self):
 	call("video/capture.sh")
