13e06de615fa4308245cf627a65ec2aaede1adda
Raspberry Pi <pi@rpi>
2016-09-15 08:21:17 -0400

    fixing goal pix.... hopefully

--- plugins/goalpic.py
+++ plugins/goalpic.py
@@ -9,5 +9,5 @@ class Plugin:
         bus.subscribe_map( 'score_goal': lambda d: self.snap()}, thread=True)
 
     def snap(self):
-        call("video/capture.sh")
+	call("video/capture.sh")
         self.bus.notify('picture_captured')
