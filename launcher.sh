46169c164341139b34bbc817f38ffaa958cdb961
Raspberry Pi <pi@rpi>
2016-09-30 08:38:22 -0400

    removed slack plugin from running, updated launcher

--- launcher.sh
+++ launcher.sh
@@ -4,7 +4,7 @@ sudo killall python3
 sudo killall chromium-browser
 cd dev/foos
 python -m SimpleHTTPServer 8000 &
-chromium-browser http://localhost:8000/web &
+chromium-browser --incognito --kiosk http://localhost:8000/web &
 sudo python3 foos.py &
 
  
