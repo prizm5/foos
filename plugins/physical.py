6a5cdb98e9c12efc8830a0aa69aefacb58118ebc
Nils Creque <nils.creque@smartstream-stp.com>
2016-09-30 13:53:36 -0400

    adding last goal decremenet on red button

--- plugins/physical.py
+++ plugins/physical.py
@@ -21,48 +21,34 @@ class Plugin():
         
         self.AddSensor(config.yellow_trigger, self.handle_yellow, config.button_debnce)
         self.AddSensor(config.black_trigger, self.handle_black, config.button_debnce)
-        
-        self.greenpush = 0
-        self.game_mode = GameState.Idle
+
 
     def process_event(self, ev):
         now = time.time()
-        if ev.name == "set_game_mode":
-            self.game_mode = GameState.Active
         if ev.name == "score_reset":
-            self.game_mode = GameState.Idle
+            self.last_goal = None 
 
     def run(self):
         while True:
             time.sleep(0.01)
     
     def handle_yellow(self, channel):
+        self.last_goal = 'yellow'
         self.bus.notify('goal_event', {'source': 'serial', 'team': 'yellow', 'duration': 100001})
         
     def handle_black(self, channel):
+        self.last_goal = 'black'
         self.bus.notify('goal_event', {'source': 'serial', 'team': 'black', 'duration': 100001})
     
     def handle_red_button(self, channel):
+        if self.last_goal != None:
+            self.bus.notify('decrement_score', {'team': self.last_goal})
+
+    def handle_green_button(self, channel):
         self.bus.notify("set_game_mode", {"mode": 10 })
         self.bus.notify("reset_score")
         self.bus.notify("set_players",{'black':[{'name':'Player 1','station':'Gladstone'}, {'name':'Player 2','station':'Queen Lane'}], 
                                       'yellow':[{'name':'Player 3','station':'Strafford'}, {'name':'Player 4','station':'Penllyn'}]})
-        # self.bus.notify("set_players",{'black':['Player 1','Player 2'], 'yellow':['Player 3','Player 4']})
-        # start = {
-        #         "black":["Player 1","Player 2"], 
-        #         "yellow":["Player 3","Player 4"],
-        #         "mode": 10,
-        #         "stations": [ { "name": "Faiz", "station": "Secane" },
-        #                       { "name": "Nilhouse", "station": "Gladstone" } ]
-        #         } 
-        # self.bus.notify("start_game",start )
-        
-    def handle_green_button(self, channel):
-        if self.game_mode == GameState.Active and self.greenpush == 1 :
-           self.bus.notify('goal_event', {'source': 'serial', 'team': 'yellow', 'duration': 100001})
-           self.greenpush += 1 
-        else:
-            self.greenpush = 0
 
 
     def AddSensor(self, port, callback, bounce):
--- web/src/score.jsx
+++ web/src/score.jsx
@@ -29,21 +29,21 @@ class Score extends React.Component {
     return (
       <div className="score-row">
         <div className={"left left-score " + (this.statusClass('team1'))}>
-          <div className="black number-container">
-            {this.props.score.team1}
-          </div>
-          <div className="names shadowed">
-            <p>{this.props.game.team1[0].name}</p>
-            <p>{this.getPlayerName(this.props.game.team1[1])}</p>
-          </div>
-        </div>
-        <div className={this.statusClass('team2')}>
           <div className="number-container">
-            {this.props.score.team2}
+            {this.props.score.team1}
           </div>
           <div className="names shadowed">
             <p>{this.props.game.team2[0].name}</p>
             <p>{this.getPlayerName(this.props.game.team2[1])}</p>
+          </div>
+        </div>
+        <div className={this.statusClass('team2')}>
+          <div className="black number-container">
+            {this.props.score.team2}
+          </div>
+          <div className="names shadowed">
+            <p>{this.props.game.team1[0].name}</p>
+            <p>{this.getPlayerName(this.props.game.team1[1])}</p>
           </div>
         </div>
       </div>
